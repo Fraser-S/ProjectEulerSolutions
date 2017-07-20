0191dedf2bf3c9289b1c56de92a3b6d733b397c0
Fraser Sprott <frasers@hotmail.co.uk>
2017-07-20 10:24:49 +0100

    code worked, used the wrong input for the test.

--- src/test/scala/Project1test.scala
+++ src/test/scala/Project1test.scala
@@ -2,7 +2,15 @@ import org.scalatest._
 
 class Project1test extends FlatSpec with Matchers {
   val project1 = new Project1()
-  "The notation function with short" should "follow the correct notation for less than 10^3" in {
-    assert(project1.sumMultiplesOf3And5(10) === 23)
+  "sumMultiplesOf3And5" should "should add the numbers between 1-9 that are multiples of 3 and 5" in {
+    assert(project1.sumMultiplesOf3And5(9) === 23)
+  }
+
+  "it" should "should add the numbers between 1-20 that are multiples of 3 and 5" in {
+    assert(project1.sumMultiplesOf3And5(20) === 3+5+6+9+10+12+15+18+20)
+  }
+
+  "it" should "should add the numbers between 1-50 that are multiples of 3 and 5" in {
+    assert(project1.sumMultiplesOf3And5(50) === 3+5+6+9+10+12+15+18+20+21+24+25+27+30+33+35+36+39+40+42+45+48+50)
   }
 }
